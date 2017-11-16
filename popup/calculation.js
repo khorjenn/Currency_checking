26b3901d93730d8654e695e19018e55f79ec300a
tohminh99 <tohminh99@yahoo.com>
2017-11-16 14:56:15 +0800

    Added currency name function

--- popup/calculation.js
+++ popup/calculation.js
@@ -1,6 +1,8 @@
 document.getElementById("currencyCal").onclick = function(){
     var from = document.getElementById('from').value;
+    var from_data = from.substring(0,3);
     var to = document.getElementById('to').value;
+    var to_data = to.substring(0,3);
     var amount = document.getElementById('amount').value;
 
     var xHttp = new XMLHttpRequest();
@@ -8,12 +10,12 @@ document.getElementById("currencyCal").onclick = function(){
     xHttp.onreadystatechange = function(){
         if(xHttp.readyState == 4 && xHttp.status == 200){
             var obj = JSON.parse(this.responseText);
-            var fact = obj.rates[to];
+            var fact = obj.rates[to_data];
             var total = parseFloat(amount) * parseFloat(fact);
             
             document.getElementById('result').innerHTML = total;
         }
     }
-    xHttp.open('GET', 'https://api.fixer.io/latest?base='+from+'&symbols='+to, true);
+    xHttp.open('GET', 'https://api.fixer.io/latest?base='+from_data+'&symbols='+to_data, true);
     xHttp.send();
 };
